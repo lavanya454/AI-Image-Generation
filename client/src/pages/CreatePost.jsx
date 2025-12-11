3d0d98cf8f60f168b89f0274e7f1b16aed4ff628
lavanya454 <165484104+lavanya454@users.noreply.github.com>
2025-12-11 13:47:51 +0530

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -21,7 +21,7 @@ const CreatePost = () => {
       setLoading(true);
     
     try {
-      const response =await fetch('http://localhost:8080/api/v1/post',{
+      const response =await fetch('https://ai-image-generation-7cye.onrender.com/api/v1/post',{
         method:'POST',
         headers:{
           'Content-Type':'application/json',
@@ -61,7 +61,7 @@ const CreatePost = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch("http://localhost:8080/api/v1/dalle/", {
+      const response = await fetch("https://ai-image-generation-7cye.onrender.com/api/v1/dalle/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
